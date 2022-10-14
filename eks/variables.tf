e6e43b8c3856c099e3e90081bba2d94b82693440
karanbirsingh7 <karanbirsingh1161@gmail.com>
2022-10-13 21:42:28 -0400

    add: variables

---
+++ eks/variables.tf
@@ -0,0 +1,17 @@
+variable "eks-cluster-name" {
+  default     = "malordekscluster"
+  type        = string
+  description = "name for the EKS cluster"
+}
+
+variable "eks-version" {
+  description = "AWS Kubernetes version"
+  default     = 0
+  type        = number
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
